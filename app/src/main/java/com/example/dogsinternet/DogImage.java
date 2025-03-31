c203b8ea96855b80b441cc889039ee42a1fed6e3
ri-gusev <gusev.roman1@mail.ru>
2025-04-01 01:27:53 +0300

    Now we can catch a JSON from the url

---
+++ app/src/main/java/com/example/dogsinternet/DogImage.java
@@ -0,0 +1,20 @@
+package com.example.dogsinternet;
+
+public class DogImage {
+
+    private String message;
+    private String status;
+
+    public DogImage(String message, String status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- app/src/main/java/com/example/dogsinternet/MainActivity.java
+++ app/src/main/java/com/example/dogsinternet/MainActivity.java
@@ -1,24 +1,47 @@
 package com.example.dogsinternet;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        loadDogImage();
+    }
+
+    private void loadDogImage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(BASE_URL);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    InputStream inputStream = urlConnection.getInputStream(); //побайтово
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream); //посимвольно
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader); //построчно
+                    String result = bufferedReader.readLine();
+                    Log.d("MainActivity", result);
+                } catch (Exception e) {
+                    Log.d("MainActivity", "incorrect url");
+                }
+            }
+        }).start();
     }
 }
