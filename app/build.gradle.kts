f19973f198b30f476297697ea9d2484f8c72ed53
ri-gusev <gusev.roman1@mail.ru>
2025-04-02 01:39:47 +0300

    refactoring(add RxJava and MVVM architecture)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation ("io.reactivex.rxjava3:rxandroid:3.0.2")
+    implementation ("io.reactivex.rxjava3:rxjava:3.1.5")
 }
--- app/src/main/java/com/example/dogsinternet/MainActivity.java
+++ app/src/main/java/com/example/dogsinternet/MainActivity.java
@@ -5,61 +5,29 @@ import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
-import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
+    private static final String TAG = "MainActivity";
+
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        loadDogImage();
-    }
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
-    private void loadDogImage() {
-        new Thread(new Runnable() {
+        viewModel.loadDogImage();
+        viewModel.getDogImage().observe(this, new Observer<DogImage>() {
             @Override
-            public void run() {
-                try {
-                    URL url = new URL(BASE_URL);
-                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                    InputStream inputStream = urlConnection.getInputStream(); //побайтово
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream); //посимвольно
-                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader); //построчно
-
-                    //тк string - иммутабельный мы используем StringBuilder и считываем json в Stringbuilder
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String result;
-                    do {
-                        result = bufferedReader.readLine();
-                        if (result != null){
-                            stringBuilder.append(result);
-                        }
-                    } while (result != null);
-
-                    JSONObject jsonObject = new JSONObject(stringBuilder.toString());
-                    String message = jsonObject.getString("message");
-                    String status = jsonObject.getString("status");
-
-                    DogImage dogImage = new DogImage(message, status);
-
-                    Log.d("MainActivity", dogImage.toString());
-                } catch (Exception e) {
-                    Log.d("MainActivity", "incorrect url");
-                }
+            public void onChanged(DogImage dogImage) {
+                Log.d(TAG, dogImage.toString());
             }
-        }).start();
+        });
     }
 }
